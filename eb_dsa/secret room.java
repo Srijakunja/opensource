c07605940716a29315f1fecba808a83ca5c9b56c
Srijakunja <238r1a05v2@cmrec.ac.in>
2024-11-13 21:27:10 +0530

    Create secret room.java

---
+++ eb_dsa/secret room.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String key1=sc.nextLine();
+        String key2=sc.nextLine();
+        if(key1.equals(key2)){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
